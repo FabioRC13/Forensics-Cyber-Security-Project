008a634046cb7d4d55a886124a9c89f833a0912b
1cgmr <cgmr.321@gmail.com>
2015-11-08 20:15:37 +0000

    componete com metodos de manipulação de imagem itegrada com a interface. Adicionada opçõ de saveas na interface

--- LSB.py
+++ LSB.py
@@ -4,13 +4,13 @@ import binascii
 
 
 
-#Create a new image
+# Create a new image
 img = Image.open('Lenna.jpg')
 
-#Create the pixel map
-pixels = img.load() 
+# Create the pixel map
+pixels = img.load()
 
- # Convert message  into bits
+# Convert message  into bits
 def convert_message_to_binary(message):
     result = ''
     for c in message:
@@ -18,6 +18,7 @@ def convert_message_to_binary(message):
         bits = '00000000'[len(bits):] + bits
         result = result + bits
     return result
+
 
 #Convert a decimal number to binary
 
@@ -32,14 +33,22 @@ def convert_decimal_binary(value):
         value /= 2
     return s
 
+def utf8_to_bin(text):
+    return '{:b}'.format(int(text.encode('utf-8').encode('hex'), 16))
+
+
+def bin_to_utf8(bits):
+    ('%x' % int(bits, 2)).decode('hex').decode('utf-8')
+
 #Convert bits to text
 
 def convert_bits_text(bits, encoding='utf-8', errors='surrogatepass'):
     chars = []
     for b in range(len(bits) / 8):
-        byte = bits[b*8:(b+1)*8]
+        byte = bits[b * 8:(b + 1) * 8]
         chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
     return ''.join(chars)
+
 
 #Auxiliar method of the method convert_bits_text
 
@@ -48,44 +57,44 @@ def int_to_bytes(value):
     n = len(hex_string)
     return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
+
 # Method to get the last bit
 
 def get_last_bit(binarynumber):
+    lastbit = len(binarynumber) - 1
 
-   	lastbit = len(binarynumber)-1
+    return binarynumber[lastbit]
 
-	return binarynumber[lastbit]
 
 #Replace the last bit of RGB for a bit of the encypted message
 
 def replaceBit(messageConverted, bitposition):
-
-    index=0
+    index = 0
     bitpositionsize = (len(bitposition))
     newbit = " "
 
-    while(index < bitpositionsize):
-        
-        if  index != bitpositionsize-1:
-            
-           newbit = newbit + newbit.join(bitposition[index])
+    while (index < bitpositionsize):
+
+        if index != bitpositionsize - 1:
+
+            newbit = newbit + newbit.join(bitposition[index])
 
         else:
-            
+
             newbit = newbit + newbit.join(messageConverted[0])
 
         index = index + 1
-   
+
     return newbit
+
 
 # Hide the size of message in the first pixel
 def hide_size(message):
-
     messagesize = len(message)
     global messagesizeinbits
     messagesizeinbits = convert_decimal_binary(messagesize)
 
-    rgbValue = pixels[0,0]
+    rgbValue = pixels[0, 0]
     """Um tupulo e um tipo imutaveis por isso tem de ser convertido numa lista por forma a ser manipulado"""
 
     rgbValue = list(rgbValue)
@@ -93,7 +102,6 @@ def hide_size(message):
     blueValue = messagesizeinbits[0:8]
     greenValue = messagesizeinbits[8:16]
     redValue = messagesizeinbits[16:24]
-
 
     rgbValue[2] = int(blueValue, 2)
 
@@ -108,12 +116,11 @@ def hide_size(message):
         rgbValue[0] = 0
 
     rgbValue = tuple(rgbValue)
-    pixels[0,0] = rgbValue
+    pixels[0, 0] = rgbValue
 
 
 def get_hide_size(pixels):
-
-    rgbValue = pixels[0,0]
+    rgbValue = pixels[0, 0]
 
     redValue = convert_decimal_binary(rgbValue[0])
     greenValue = convert_decimal_binary(rgbValue[1])
@@ -123,14 +130,16 @@ def get_hide_size(pixels):
     sizeInt = int(sizeBits, 2)
     return sizeInt
 
+
 #Hide the message in the image
 
 def hide_message(img, message):
-
     messageConverted = convert_message_to_binary(message)
     #print message
-    print messageConverted
+    #print messageConverted
     size = len(messageConverted)
+    #print size
+    #print "======================"
 
     hide_size(message)
 
@@ -142,7 +151,7 @@ def hide_message(img, message):
 
             if len(messageConverted) > 0:
 
-                rgbValue = pixels[i,j]
+                rgbValue = pixels[i, j]
                 #print messageConverted
                 redValue = convert_decimal_binary(rgbValue[0])
                 greenValue = convert_decimal_binary(rgbValue[1])
@@ -151,23 +160,23 @@ def hide_message(img, message):
                 #print pixels[i,j], redValue, greenValue, blueValue
 
                 if len(messageConverted) > 0:
-                	redValue = replaceBit(messageConverted, redValue) 
-                	messageConverted = update(messageConverted)
+                    redValue = replaceBit(messageConverted, redValue)
+                    messageConverted = update(messageConverted)
 
                 if len(messageConverted) > 0:
-                	greenValue = replaceBit(messageConverted, greenValue) 
-                	messageConverted = update(messageConverted)
+                    greenValue = replaceBit(messageConverted, greenValue)
+                    messageConverted = update(messageConverted)
 
                 if len(messageConverted) > 0:
-                	blueValue = replaceBit(messageConverted,  blueValue) 
-               		messageConverted = update(messageConverted)
+                    blueValue = replaceBit(messageConverted, blueValue)
+                    messageConverted = update(messageConverted)
 
                 #print "(",int(redValue,2),",", int(greenValue,2), ",",int(blueValue,2), ")",  redValue, greenValue, blueValue
-               	redValue = int(redValue,2)
-                greenValue = int(greenValue,2)
-                blueValue = int(blueValue,2)
+                redValue = int(redValue, 2)
+                greenValue = int(greenValue, 2)
+                blueValue = int(blueValue, 2)
 
-            	pixels[i, j] = (redValue, greenValue, blueValue)
+                pixels[i, j] = (redValue, greenValue, blueValue)
 
 
                 #print "==========================="
@@ -175,17 +184,16 @@ def hide_message(img, message):
 
 #Update the converted message
 def update(messageConverted):
+    return messageConverted[1:]
 
-	return messageConverted[1:]
 
 #Extract the message from the image
 def extract_message(img):
-
-    pixels = img.load() 
+    pixels = img.load()
 
     messagesize = get_hide_size(pixels)
     extracted_binary_message = ''
-   # print messagesize
+    #print messagesize
 
     end = False
     for i in range(img.size[0]):
@@ -194,13 +202,14 @@ def extract_message(img):
             if i == 0 and j == 0:
                 continue
 
-            rgbValue = pixels[i,j]
+            rgbValue = pixels[i, j]
             redValue = convert_decimal_binary(rgbValue[0])
             greenValue = convert_decimal_binary(rgbValue[1])
             blueValue = convert_decimal_binary(rgbValue[2])
-            extracted_binary_message = extracted_binary_message + get_last_bit(redValue) + get_last_bit(greenValue) + get_last_bit(blueValue)
+            extracted_binary_message = extracted_binary_message + get_last_bit(redValue) + get_last_bit(
+                greenValue) + get_last_bit(blueValue)
             #print extracted_binary_message
-            if len(extracted_binary_message) > messagesize*8:
+            if len(extracted_binary_message) > messagesize * 8:
                 end = True
                 break
         if end:
@@ -209,19 +218,20 @@ def extract_message(img):
     message = convert_bits_text(extracted_binary_message)
 
     return message
-    
-###### TESTES ########
-hide_message(img, "temos de ter uma mensagem enorme para poder realmente verificar se esta a aconntecer alguma coisa com a imagem. nao se pode ter uma frase ou duas :D")
 
-print extract_message(img)
+###### TESTES ########
+#largeutf8string = "Convert text into ASCII number format. For example A is 065. Text in a computer is stored as numbers called ASCII numbers with each letter having its own number. Input text to convert to these ASCII numbers. ASCII is short for American Standard Code for Information Interchange. With applications in computers and other devices that use text, ASCII codes represent text. Based on the English alphabet, ASCII is a character-encoding scheme. ASCII was originally developed from telegraphic codes.Computers can only understand numbers, and ASCII codes are numerical representations of characters that a computer can understand. Examples of characters are a, 1, or >. For example, 097 is the ASCII numerical representation of the character a. ASCII covers over 100 characters with some of these characters being control characters that control how text appears.Work on ASCII began in the 1960s through a committee of the American Standards Association. Many of todays character-encoding schemes are based on ASCII, plus they include additional characters. At one time ASCII was used on the World Wide Web as the most commonly used character encoding. If you prepare a text in ASCII format, you will get plain text with no format such as bold, and any computer can understand the format. Other schemes such as HTML cover formatting."
+#samllString = "temos de ter uma mensagem enorme para poder realmente verificar se esta a aconntecer alguma coisa com a imagem. nao se pode ter uma frase ou duas :D"
+
+#hide_message(img, samllString)
+#print extract_message(img)
+
+#var = utf8_to_bin(samllString)
+#print var
 
 #tamanho = "temos de ter uma mensagem enorme para poder realmente verificar se esta a aconntecer alguma coisa com a imagem. nao se pode ter uma frase ou duas :D"
 #print get_hide_size("101010101011101")
 #img.show()
-
-
-
-
 
 
 
--- interface.py
+++ interface.py
@@ -4,8 +4,12 @@ import Tkinter
 from Tkinter import *
 from tkFileDialog import *
 import tkFileDialog
+import tkMessageBox
 import numpy as np
 from PIL import Image, ImageTk
+import LSB
+
+current_full_size_image = None
 
 def startGUI():
     global master
@@ -22,28 +26,61 @@ def onOpen():
     filename = dlg.show()
     fn = filename
     #print fn #prints filename with path here
-    setImage(fn)
-
-
-def setImage(fn):
-    global img
     img = Image.open(fn)
+    setImage(img)
+
+def onSaveAs():
+    ftypes = [
+    ('Windows Bitmap','*.bmp'),
+    ('Portable Network Graphics','*.png'),
+    ('JPEG / JFIF','*.jpg'),
+    ('CompuServer GIF','*.gif'),
+    ]
+    fileName = tkFileDialog.asksaveasfile(parent=master,filetypes=ftypes ,title="Save the image as...", mode='w',defaultextension='.jpg')
+    global current_full_size_image
+    print fileName.name
+    current_full_size_image.save(fileName.name, quality=100)
+
+
+
+def setImage(img):
+    global current_full_size_image
+    current_full_size_image = img
     l, h = img.size
-    print l, h
-    print int(250), float(250) / float(l)
-    img = img.resize((int(250), int((float(250) / float(l)) * h)), Image.ANTIALIAS)
-    # text = str(2*l+100)+"x"+str(h+50)+"+0+0"
-    #print text
-    #master.geometry(text)
-    photo = ImageTk.PhotoImage(img)
+    #print l, h
+    #print int(250), float(250) / float(l)
+    img_resize = img.resize((int(250), int((float(250) / float(l)) * h)), Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(img_resize)
     label1.configure(image=photo)
     label1.image = photo  # keep a reference!
-
 
 def donothing():
     filewin = Toplevel(master)
     button = Button(filewin, text="Do nothing button")
     button.pack()
+
+def dialog_box(msg):
+    tkMessageBox.showwarning("Warning", msg)
+
+def hide_procedure():
+    global current_full_size_image
+    message = e1.get()
+    if message == "":
+        dialog_box("Please write your message")
+        return
+    #if current_full_size_image equal None:
+        #dialog_box("No image selected")
+        #return
+
+    print e1.get()
+    LSB.hide_message(current_full_size_image, e1.get())
+    dialog_box("DONE")
+    setImage(LSB.img)
+
+def extract_message():
+    global current_full_size_image
+    message = LSB.extract_message(current_full_size_image)
+    dialog_box(message)
 
     ############################## Interface Code ################################
 
@@ -55,6 +92,7 @@ def intitGui():
     master.geometry('%dx%d+0+0' % (300,400))
 
     global label1 #e global para poder ser acedida noutas funcoes
+    global e1
 
     label1 = Tkinter.Label(border=25)
     #label1.grid(row = 0, column = 0)
@@ -64,19 +102,17 @@ def intitGui():
     master.config(menu=menubar)
 
     ############################## Tool Bar ################################
-    filemenu.add_command(label="New", command=donothing)
+    #filemenu.add_command(label="New", command=donothing)
     filemenu.add_command(label="Open", command=onOpen)
-    filemenu.add_command(label="Save", command=donothing)
-    filemenu.add_command(label="Save as...", command=donothing)
-    filemenu.add_command(label="Close", command=donothing)
-
+    #filemenu.add_command(label="Save", command=donothing)
+    filemenu.add_command(label="Save as...", command=onSaveAs)
+    filemenu.add_command(label="Close", command=master.quit)
     filemenu.add_separator()
 
     filemenu.add_command(label="Exit", command=master.quit)
     menubar.add_cascade(label="File", menu=filemenu)
     editmenu = Menu(menubar, tearoff=0)
     editmenu.add_command(label="Undo", command=donothing)
-
     editmenu.add_separator()
 
     editmenu.add_command(label="Cut", command=donothing)
@@ -84,8 +120,8 @@ def intitGui():
     editmenu.add_command(label="Paste", command=donothing)
     editmenu.add_command(label="Delete", command=donothing)
     editmenu.add_command(label="Select All", command=donothing)
-
     menubar.add_cascade(label="Edit", menu=editmenu)
+
     helpmenu = Menu(menubar, tearoff=0)
     helpmenu.add_command(label="Help Index", command=donothing)
     helpmenu.add_command(label="About...", command=donothing)
@@ -95,18 +131,19 @@ def intitGui():
 
     #button1 = Button(master, text="Select the image", command=search_img)
     e1 = Entry(master, text="Write your message")
-    button2 = Button(master, text='Hide your message')
-    #button3 = Button(master, text='Exit',  command=master.quit)
+    button2 = Button(master, text='Hide your message', command=hide_procedure)
+    button3 = Button(master, text='Extract message',  command=extract_message)
 
 
     #button2.grid(row=1)
     button2.grid(row=1)
     e1.grid(row=2)
-    #button3.grid(row=3)
+    button3.grid(row=3)
     #e1.grid(row=4)
 
     # ola = convert_message_to_binary("sdfsd")
     #print ola
 
-if __name__ == "__main__":
-    startGUI()
+#if __name__ == "__main__":
+
+startGUI()
